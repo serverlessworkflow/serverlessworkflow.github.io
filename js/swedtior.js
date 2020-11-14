dd3d27054a82c31377a4dfb7300acbe48e339a76
Tihomir Surdilovic <tsurdilo@redhat.com>
2020-11-14 10:56:55 -0500

    update diagram service endpoint to use https (#29)
    Signed-off-by: Tihomir Surdilovic <tsurdilo@redhat.com>

--- js/swedtior.js
+++ js/swedtior.js
@@ -442,7 +442,7 @@ function generateDiagram() {
     var modelVal = model.getValue();
 
 
-    callUrl('POST', 'http://diagram-service-mmagnani.apps.kogito-cloud.automation.rhmw.io/swdiagram', modelVal, function showDiagram(res){
+    callUrl('POST', 'https://diagram-service-secure-mmagnani.apps.kogito-cloud.automation.rhmw.io/swdiagram', modelVal, function showDiagram(res){
         if (res !== null) {
             var container = document.querySelector(".workflowdiagram");
             container.innerHTML = res;
