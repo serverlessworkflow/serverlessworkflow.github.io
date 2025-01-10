c371dfce587382cccb161f58210922f45f6eb989
Jean-Baptiste Bianchi <jb.bianchi@neuroglia.io>
2025-01-10 10:53:10 +0100

    Spec errors support - Added dynamic routes to support specification errors Notes: - Return type based on Accept header - Enabled SSR on netlify
    Closes #200
    
    Signed-off-by: Jean-Baptiste Bianchi <jb.bianchi@neuroglia.io>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,4 +19,5 @@ export default defineConfig({
       wrap: true,
     },
   },
+  adapter: netlify(),
 });
