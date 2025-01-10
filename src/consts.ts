7501ad6c09f0ff06a9279efd876efbbb6d205415
Jean-Baptiste Bianchi <jb.bianchi@neuroglia.io>
2025-01-10 13:42:49 +0100

    Add SDK/Runtime CTA - Added SDKs & Runtime call to action - Added sitemap link
    Signed-off-by: Jean-Baptiste Bianchi <jb.bianchi@neuroglia.io>

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,8 @@
 export const DEFAULT_TITLE = 'Serverless Workflow';
 export const DEFAULT_DESC = `Serverless Workflow presents a vendor-neutral, open-source, and entirely community-driven ecosystem tailored for defining and executing DSL-based workflows in the realm of Serverless technology.`;
 
-export const GITHUB_LINK = 'https://github.com/serverlessworkflow/specification/';
+export const GITHUB_ORG = 'https://github.com/serverlessworkflow/';
+export const GITHUB_LINK = GITHUB_ORG + 'specification/';
 export const SLACK_LINK = 'https://slack.cncf.io/';
 export const X_TWITTER_LINK = 'https://x.com/cncfworkflow';
 export const LINKEDIN_LINK = 'https://www.linkedin.com/company/serverless-workflow/posts';
