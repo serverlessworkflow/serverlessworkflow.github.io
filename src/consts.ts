e79b05d2362bc99729ceae14abedd2bfdd27d6cb
Ricardo Zanini <ricardozanini@gmail.com>
2026-06-18 10:03:07 -0400

    Rename to Open Workflow Specification
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
-export const DEFAULT_TITLE = 'Serverless Workflow';
-export const DEFAULT_DESC = `Serverless Workflow presents a vendor-neutral, open-source, and entirely community-driven ecosystem tailored for defining and executing DSL-based workflows in the realm of Serverless technology.`;
+export const DEFAULT_TITLE = 'Open Workflow Specification';
+export const DEFAULT_DESC = `Open Workflow Specification presents a vendor-neutral, open-source, and entirely community-driven ecosystem for defining and executing DSL-based workflows.`;
 
 export const CURRENT_VERSION = '1.0.3';
 
 export const GITHUB_ORG = 'https://github.com/serverlessworkflow/';
 export const GITHUB_LINK = GITHUB_ORG + 'specification/';
-export const SLACK_LINK = 'https://slack.cncf.io/';
+export const SLACK_LINK = 'https://cloud-native.slack.com/messages/open-workflow';
 export const X_TWITTER_LINK = 'https://x.com/cncfworkflow';
 export const LINKEDIN_LINK = 'https://www.linkedin.com/company/serverless-workflow/posts';
 export const DEV_MAILING_LIST_LINK = 'mailto:cncf-serverlessws-maintainers@lists.cncf.io';
